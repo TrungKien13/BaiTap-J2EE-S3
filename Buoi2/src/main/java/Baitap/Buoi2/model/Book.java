1ce38c9b3b99106dec84dc0738cd64e5373095f1
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2026-01-31 11:06:38 +0700

    Add files via upload

---
+++ Buoi2/src/main/java/Baitap/Buoi2/model/Book.java
@@ -0,0 +1,24 @@
+package Baitap.Buoi2.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor
+@Setter
+
+public class Book {
+    private int id;
+    private String title;
+    private String author;
+
+    public int getId() {return id;}
+    public void setId(int id) { this.id = id; }
+    public String getTitle() { return title; }
+    public void setTitle(String title) { this.title = title; }
+    public String getAuthor() { return author; }
+    public void setAuthor(String author) { this.author = author; }
+}
