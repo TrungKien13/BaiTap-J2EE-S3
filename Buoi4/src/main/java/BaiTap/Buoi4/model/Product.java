6fed2b24aab30cff2c9b02f4ffd67cd8ee2be2cd
TrungKien13 <133593440+TrungKien13@users.noreply.github.com>
2026-02-07 10:36:49 +0700

    Add files via upload

---
+++ Buoi4/src/main/java/BaiTap/Buoi4/model/Product.java
@@ -0,0 +1,79 @@
+package BaiTap.Buoi4.model;
+
+import jakarta.validation.constraints.*;
+import lombok.*;
+import org.hibernate.validator.constraints.Length;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private int id;
+
+    @NotBlank(message = "Tên sản phẩm không được để trống")
+    private String name;
+
+    @Length(max = 200, message = "Tên hình ảnh không quá 200 kí tự")
+    private String image;
+
+    @NotNull(message = "Giá sản phẩm không được để trống")
+    @Min(value = 1, message = "Giá sản phẩm không được nhỏ hơn 1")
+    @Max(value = 9999999, message = "Giá sản phẩm không được lớn hơn 9999999")
+    private long price;
+
+    private Category category;
+
+    public Product(int id, String name, String image, long price, Category category) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.category = category;
+    }
+
+    public Product() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public long getPrice() {
+        return price;
+    }
+
+    public void setPrice(long price) {
+        this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+
+}
